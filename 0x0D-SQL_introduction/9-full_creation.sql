08e2b3a3965ca1095997f14f6094a7426e00e6bb
Opeyemi Richard <opeyemirichard@gmail.com>
2024-02-16 10:14:18 -0800

    Create table and add records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Create table and insert values
+
+INSERT INTO second_table((id, name, score)
+VALUES((1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8))
+WHERE TABLE = (
+	CREATE TABLE second_table(id INT, name VARCHAR(256), score INT));
