e286673458e286c6550a9bbaf15ad51b7a6fefde
Opeyemi Richard <opeyemirichard@gmail.com>
2024-02-17 10:35:59 -0800

    Create table with constraints and Forefign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Create table with constraints
-USE hbtn_0d_usa;
+USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT NOT NULL PK,
 	state_id INT NOT NULL,
