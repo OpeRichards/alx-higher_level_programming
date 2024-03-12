2ba7f924f2a4f8033ea34de487fe9f7672ad5c4f
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-12 09:15:38 +0100

    Compare last digit of random signed number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = str(number)
-last_digit = digit[-1]
-last_digit = int(last_digit)
-if last_digit > 5:
-    print(f'Last digit of {number} is {last_digit} and is greater than 5')
-elif last_digit == 0:
+if number > 0:
+    digit = number % 10
+else:
+    digit = number % (-10)
+    
+if digit > 5:
+    print(f'Last digit of {number} is {digit} and is greater than 5')
+elif digit == 0:
     print(f'Last digit of {number} and is 0')
 else:
-    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
+    print(f'Last digit of {number} is {digit} and is less than 6 and not 0')
