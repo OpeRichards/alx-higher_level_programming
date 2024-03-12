f8f2d28098844e3cb045255d2004b32bc56f9bf1
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-12 10:46:02 +0100

    Print lower case alphabets using ascii

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z') + 1):
-    print(f'{alphabet:c}', end='')
+for alphabet in range(97, 123):
+    print('{}'.format(chr(alphabet)), end='')
