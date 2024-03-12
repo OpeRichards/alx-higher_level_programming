c93e47ced70208bd6a2d103b96420273c73b01bd
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-12 15:05:25 +0100

    All combinations of 2 digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,8 @@ for num in range(0, 9):
     for numb in range(1, 10):
         if num > numb:
             continue
+        elif num == numb:
+            print(''.format(), end='')
         elif num == 8 and numb == 9:
             print('{}{}'.format(num, numb))
         else:
