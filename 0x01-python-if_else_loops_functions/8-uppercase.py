84490f9f3c3c99038cb8b3bf7416bfcb0c80436e
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-19 21:22:55 +0100

    Print only in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            c = chr(ord(i) - 32)
+    for y in str:
+        if ord(y) >= ord('a') and ord(y) <= ord('z'):
+            alph = chr(ord(y) - 32)
         else:
-            c = i
-        print("{}".format(c), end="")
+            alph = y
 
+        print('{}'.format(alph), end='')
+    print()
