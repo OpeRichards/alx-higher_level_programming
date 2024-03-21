b8ad4b072e78c31b4f1defba3deb9b4aed8994b2
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-21 15:27:50 +0100

    Return an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        print('None')
-    if idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         print('None')
     else:
-        return my_list.pop(idx)
+        return my_list[idx]
