dd2db6ee786c39a1dc54c1cb76b8573e7c8830bf
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-23 11:27:41 +0100

    Adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_2a = tuple_a + (0, 0)
-    tuple_2b = tuple_b + (0, 0)
-    sum_tuple = tuple([tuple_2a[xrs] + tuple_2b[xrs] for xrs in range(len(tuple_a))])
+    tuple_2a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    tuple_2b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+    sum_tuple = tuple([tuple_2a[xrs] + tuple_2b[xrs] for xrs in range(2)])
     return sum_tuple
