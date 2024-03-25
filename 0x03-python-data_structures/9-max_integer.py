5538e109e2aaf34f95f9ed2d82333a7c0e4bbc2e
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-25 21:33:05 +0100

    Finds maximum number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return "None"
-    else:    
+    else:
         max_num = my_list[0]
         for num in range(1, len(my_list)):
             if my_list[num] > max_num:
