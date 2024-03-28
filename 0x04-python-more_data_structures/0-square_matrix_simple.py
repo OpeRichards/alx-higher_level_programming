497993c6b86ee0aad279ca8c07278e7f3ac146ab
Opeyemi Richard <opeyemirichard@gmail.com>
2024-03-28 10:00:46 +0100

    Square of a nested matrix integers

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for elem in matrix:
+        new_row = []
+        for x in elem:
+            new_row.append(x**2)
+        new_matrix.append(new_row)    
+    return new_matrix
