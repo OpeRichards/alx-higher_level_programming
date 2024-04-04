bc227c493ee5d310b037c87ecc7cd53fb4601e11
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-04 12:47:25 +0100

    List division by list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print('wrong type')
             result = 0
         except ZeroDivisionError:
-            print('out of range')
+            print('division by 0')
             result = 0
         except IndexError:
             print('out of range')
