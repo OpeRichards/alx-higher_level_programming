583653134c09258f2ff57d83f9c6858be9215a22
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-08 16:25:52 +0100

    Create a Square class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Declare a class Square."""
+
+
 class Square:
-    """Declare a class Square."""
+    """Square has equal sizes.
+    However, this is an empty square class"""    
     pass
