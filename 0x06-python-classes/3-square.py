4b076d5224d9bf273d0a00dcb47326d3ac8b45c1
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-10 20:16:55 +0100

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Declare a class Square."""
+
+
+class Square:
+    """A square is a four four sided shape.
+    It has equal sides."""
+    def __init__(self, size=0):
+        """Initialize the size of square.
+        int(size): size of a side of square."""
+        if size != int(size):
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
