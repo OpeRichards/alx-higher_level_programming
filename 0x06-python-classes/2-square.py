374bc907964de696636d8452764ffce157503e2c
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-10 16:59:59 +0100

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Declare a class Square."""
+
+
+class Square:
+    """A square is a four four sided shape.
+    It has equal sides."""
+    def __init__(self, size=0):
+        """Initialize the size of square.
+        Args:
+            int(size): a side of the square
+        Returns:
+            None
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
