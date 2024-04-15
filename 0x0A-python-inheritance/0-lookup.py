d63ccfeba21ec00f2cac19c1e890f4e6bfe8dca3
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-15 16:09:52 +0100

    List all in-built and user-defined attributes of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""A fucntion."""
+"""A function to display ."""
 
 
 def lookup(obj):
     """A function to lookup directory obj.
     Returns:
-        The list of available attributes and methods.
+        List all available built-in and user-defined 
+            attributes and methods of an object.
     """
     return dir(obj)
