4319420692ea2b134fcc8c50a47a5d01f284403c
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-16 14:50:04 +0100

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """A function that reads from a file."""
-    with open(filename, encoding="utf-8") as my_file:
+    with open(filename, encoding="utf-8") as myfile:
         print(my_file.read(), "")
