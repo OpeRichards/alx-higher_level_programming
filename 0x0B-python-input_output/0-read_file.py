2b542b16bc2fc4b5b8267a505fecc763f0692f4b
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-16 14:48:40 +0100

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Define a function read_file"""
+
+
 def read_file(filename=""):
+    """A function that reads from a file."""
     with open(filename, encoding="utf-8") as my_file:
         print(my_file.read())
