747bb44b286d426b9dd3a50a452424bdb14a356e
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-17 14:00:50 +0100

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,6 +3,12 @@
 
 
 class Student:
+    """A class Student that defines a student data.
+    Args:
+        first_name (str): First name of a student.
+        last_name (str): Last name of a student.
+        age (int): Age of a student.
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
