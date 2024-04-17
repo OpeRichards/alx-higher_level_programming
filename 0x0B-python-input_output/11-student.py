f4d20ca68a38e927924e50b12407369f8fbf12c3
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-17 15:17:16 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module defines class Student."""
+
+
+class Student:
+    """A class Student that defines a student data."""
+    def __init__(self, first_name, last_name, age):
+        """Initializes class Student.
+        Args:
+            first_name (str): First name of a student.
+            last_name (str): Last name of a student.
+            age (int): Age of a student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a student"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """Return a class Student to JSON with filter.
+        If a list of strings exist, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and \
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except FileNotFoundError:
+                pass
