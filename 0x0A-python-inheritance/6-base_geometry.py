9ed9c2db80c23cdfa20fe63ad954a813b82258ac
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-17 22:24:37 +0100

    Improve Geometry - exception raise

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """This is an empty class."""
+    def area(self):
+        raise Exception('area() is not implemented')
