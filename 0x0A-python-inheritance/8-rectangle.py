ca64683b985c7f01c406f904d47e5b648abcd37c
Opeyemi Richard <opeyemirichard@gmail.com>
2024-04-17 23:15:18 +0100

    Rectangle child of BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,25 +2,7 @@
 """This module defines a class BaseGeometry."""
 
 
-class BaseGeometry:
-    """This is class BaseGeometry."""
-    def area(self):
-        """Raise exception."""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """This method validates value.
-        Args:
-            name (str): Name object to validate.
-            value (int): Integer to validate.
-        Returns:
-            ValueError: Raise ValueError Exception.
-            TypeError: Raise TypeError Exception.
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
