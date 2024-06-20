1041c41eb334549177e007d1c3fdc81ef2babcf6
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-20 03:32:49 +0100

    Print elements of an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -5,5 +5,5 @@ const anArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 /* Print each element of the array using loop */
 for (let i = 0; i < anArray.length; i++) {
-  console.log(anArray[i])
+  console.log(anArray[i]);
 }
