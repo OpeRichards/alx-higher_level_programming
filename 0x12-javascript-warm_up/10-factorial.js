528d4757eae3bc907ea7aa082e4ddd25b82a6adb
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-20 05:15:19 +0100

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/* Access first argument */
+const firstArg = process.argv[2];
+
+function factorial(n) {
+  const numA = parseInt(n);
+
+  if (isNaN(numA)) {
+    return 1;
+  } else if (numA <= 1) {
+    return 1;
+  } else {
+    return numA * factorial(numA - 1);
+  }
+}
+
+/* Compute and print the factorial */
+console.log(factorial(firstArg));
