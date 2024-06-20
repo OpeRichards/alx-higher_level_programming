810569a5ee40c6d0dc39f75bad92d449b397ad26
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-20 04:34:07 +0100

    Addition of two integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+/* Access the first and second arguments */
+const a = process.argv[2];
+const b = process.argv[3];
+
+/* Add two integers */
+function add(a, b) {
+  const numA = parseInt(a);
+  const numB = parseInt(b);
+
+  if (isNaN(numA) || isNaN(numB)) {
+    return 'NaN';
+  } else {
+    return numA + numB;
+  }
+}
+
+/* Print the result of the addition */
+console.log(add(a, b));
