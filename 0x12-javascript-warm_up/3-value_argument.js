939c05e7b9c87152cb36d484093787b1c868f0c6
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-20 01:52:02 +0100

    Print first argument if available

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* Access the first argument passed */
+const firstArg = process.argv[2];
+
+/* Check if the argument is provided and print it */
+if (firstArg !== undefined) {
+    console.log(firstArg);
+} else {
+    console.log('No argument');
+}
