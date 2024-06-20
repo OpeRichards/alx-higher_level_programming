bdb51198a074ba3c0efebf1ac93607d680a06838
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-20 03:34:26 +0100

    Print elements of an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 /* Define an array to print */
-const anArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const anArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 /* Print each element of the array using loop */
 for (let i = 0; i < anArray.length; i++) {
