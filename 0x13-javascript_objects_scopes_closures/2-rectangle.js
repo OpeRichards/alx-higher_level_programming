f3a3943a9bc0e170a922d880a718a059c40cef2e
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-20 16:32:35 +0100

    Add variable conditions

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,11 @@
 
 /* Define a Rectangle class */
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+}
+module.exports = Rectangle;
