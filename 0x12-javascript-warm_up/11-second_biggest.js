dd7f1189231398918860c7360c4a3ca4dce1ba5a
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-20 12:09:10 +0100

    Print second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+/* Count number of arguments passed */
+const argCount = process.argv.length - 2
+const args = process.argv.slice(2).map(Number);
+
+/* Search for the second biggest number */
+if (argCount <= 1) {
+  console.log(0);
+} else {
+  /* Initialize the highest and second highest numbers */
+  let highest = -Infinity;
+  let secondHighest = -Infinity;
+
+  /* Loop through the arguments */
+  /* Find the highest and second highest numbers */
+  for (const num of args) {
+    if (num > highest) {
+      secondHighest = highest;
+      highest = num;
+    } else if (num > secondHighest && num != highest) {
+      secondHighest = num;
+    }
+  }
+
+  /* Print the second highest number */
+  console.log(secondHighest);
+}
