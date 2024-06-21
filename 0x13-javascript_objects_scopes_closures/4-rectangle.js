b8dac27dab29d2284a2856ad51962a7e7a4e71fe
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-21 05:52:42 +0100

    Add duble and rotate  functions to class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+/* Define an empty Rectangle class */
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    /* Instance method to print the rectangle using 'X' */
+    print () {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+
+    /* Instance method to exchange the sides of the rectangle */
+    rotate() {
+      if (this.width && this.height) {
+        [this.width, this.height] = [this.height, this.width];        }
+      }
+
+    /* Instance method to double the sides of the rectangle */
+    double() {
+      if (this.width && this.height) {
+        this.width *= 2;
+        this.height *= 2;
+      }
+    }
+  }
+  module.exports = Rectangle;
+  
