66f6d2c0977af1e25080929d22bda734c9ff99f7
Opeyemi Richard <opeyemirichard@gmail.com>
2024-06-22 11:07:16 +0100

    Count number of occurence

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,8 +2,11 @@
 
 /* Create a method that gives the length of a list */
 exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  for (let i = 0; i < list.length; i++) {
+  const newList = [];
+  const lastElement = list.length - 1
+
+  /* Iterate from the end of list to the beginning */
+  for (let i = lastElement; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
