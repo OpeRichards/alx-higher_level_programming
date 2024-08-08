1f5b6526b0e0789fca25b98f0cd8c164a01925c2
Opeyemi Richard <opeyemirichard@gmail.com>
2024-08-08 15:47:12 +0100

    Fetch & print Hello in a chosen language

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#btn_translate').click(function() {
+        const langCode = $('#language_code').val();
+        const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
+        
+        $.get(url, function(data) {
+            $('#hello').text(data.hello);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            $('#hello').text('Error: ' + textStatus + ' - ' + errorThrown);
+        });
+    });
+});
