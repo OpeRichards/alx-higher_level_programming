742a1b6e4f060380e6241d19a6fa8dbf48145fcb
Opeyemi Richard <opeyemirichard@gmail.com>
2024-08-08 11:02:00 +0100

    Update with name from url

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,8 +1,9 @@
-$.ajax({
-    method:   'GET',
-    url:    'https://swapi-api.alx-tools.com/api/people/5/?format=json',
-    success: function (data) {
-        const characterName = data.name
-        $('DIV#character').text(characterName);
-    }
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+        success: function(data) {
+            $('DIV#character').text(data.name);
+        }
+    });
 });
